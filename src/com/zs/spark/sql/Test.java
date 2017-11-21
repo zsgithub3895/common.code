024a5d8e91ccbd849361e8b750ecaf3af1fc3f78
zhangsai <zhangsai@ZS-PC>
2017-11-21 12:51:27 +0800

    SPARK sql

---
+++ src/com/zs/spark/sql/Test.java
@@ -0,0 +1,21 @@
+package com.zs.spark.sql;
+
+import com.zs.sqm.spark.redis.RedisClient;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RedisClient res = new RedisClient();
+		try {
+			//获取redis数据。*是模糊查询所有符合条件的key
+			//res.
+			//TreeSet<String> keys = res
+			//Iterator<String> ite=keys.iterator(); 
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
